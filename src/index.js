f78e1c497c6bfb31641991a4791a31ad9caaabff
nwagu chima <chima.nwagu@decagon.dev>
2022-05-03 12:22:03 +0100

    setup Login page

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import FirebaseContext from "./context/firbase";
-import { app, dataBase } from "./lib/firebase";
+import { app, dataBase, auth } from "./lib/firebase";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
-    <FirebaseContext.Provider value={{ app, dataBase }}>
+    <FirebaseContext.Provider value={{ app, dataBase, auth }}>
       <App />
     </FirebaseContext.Provider>
   </StrictMode>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firbase";
 
-const login = () => {
+const Login = () => {
+  const history = useHistory;
+  const { auth } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "Login - Insta_world";
+  }, []);
+
   return <div>I am the Login Page</div>;
 };
 
-export default login;
+export default Login;
