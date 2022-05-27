9cb489a863945ca5c7f3f1f5b4d0759728b641dc
nwagu chima <chima.nwagu@decagon.dev>
2022-05-28 00:56:07 +0100

    post header setup

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -3,6 +3,7 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import usePhotos from "../hooks/use-photos";
+import Post from "./post";
 
 function Timeline() {
   const { photos } = usePhotos();
@@ -12,7 +13,7 @@ function Timeline() {
       {!photos ? (
         <Skeleton count={4} width={640} height={500} className="mb-5" />
       ) : photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => <Post key={content.docId} content={content} />)
       ) : (
         <p className="text-center text-2xl">Follow people to see photos</p>
       )}
