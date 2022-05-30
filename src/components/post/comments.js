91177dee710378a44a073fc7cf62b1d17f34f8df
nwagu chima <chima.nwagu@decagon.dev>
2022-05-30 10:35:36 +0100

    completed commenting on post

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { formatDistance } from "date-fns";
 import { Link } from "react-router-dom";
+import AddComment from "./add-comment";
 
 export default function Comments({
   docId,
@@ -32,7 +33,12 @@ export default function Comments({
           {formatDistance(posted, new Date())} ago
         </p>
       </div>
-      <div>.</div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
     </>
   );
 }
